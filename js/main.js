72d84b1571fb3dc01f359b366fac8b5fff3c7723
jerome.rayos <jerome.rayos@maroonstudios.com>
2023-07-03 10:51:52 +0800

    Chore: code cleanup

--- js/main.js
+++ js/main.js
@@ -39,15 +39,8 @@
 		});
 	});
 
-
 	// placeholder plugin settings
 	$('input, textarea, select').placeholder()
-
-	// Back to top
-	var pxShow = 300; // height on which the button will show
-	var fadeInTime = 400; // how slow/fast you want the button to show
-	var fadeOutTime = 400; // how slow/fast you want the button to hide
-	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
 	// Show or hide the sticky footer button
 	$(window).scroll(function () {
